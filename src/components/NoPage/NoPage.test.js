52948912ed1281ef5692e61cd3c9293e1f1517b8
Frank Vielma <frankvielma@gmail.com>
2023-04-25 17:45:00 -0400

    add test for NoPage

---
+++ src/components/NoPage/NoPage.test.js
@@ -0,0 +1,31 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
+import NoPage from "./NoPage";
+
+describe("No Page", () => {
+  it("renders page not found", () => {
+    render(
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<NoPage />} />
+        </Routes>
+      </BrowserRouter>
+    );
+
+    const linkElement = screen.getByText(/page not found/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it("renders home link", () => {
+    render(
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<NoPage />} />
+        </Routes>
+      </BrowserRouter>
+    );
+
+    const linkElement = screen.getByText(/home/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+});
