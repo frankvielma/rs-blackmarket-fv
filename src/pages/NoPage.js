00a70cb7ac745f07950271cae23fd5054b2d09f3
Frank Vielma <frankvielma@gmail.com>
2023-04-25 17:54:02 -0400

    remove comments

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,19 +1,3 @@
-// import { Link } from "react-router-dom";
-// import logo from "../assets/rs_logo.png";
-
-// export default function NoPage() {
-//   return (
-//     <div>
-//       <header className="Cover-header">
-//         <img src={logo} className="Cover-logo1" alt="logo" />
-//         <div>Page not found</div>
-//         <Link to="/" className='Cover-login'>Home</Link>
-//       </header>
-//     </div>
-//   );
-// }
-
-
 import NoPage from "../components/NoPage/NoPage";
 
 export default function Home() {
