9d9f5550e08bf358d8a8ceda681bb79807211e34
Frank Vielma <frankvielma@gmail.com>
2023-04-27 10:05:48 -0400

    install and setup stylelint, prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,27 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true,
-        "jest": true
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "jsx-a11y"],
+  rules: {},
+  settings: {
+    react: {
+      version: "detect",
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:react/jsx-runtime",
-        "plugin:jsx-a11y/recommended"
-    ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-	"jsx-a11y"
-    ],
-    "rules": {
-    },
-    "settings": {
-        "react": {
-        "version": "detect"
-        }
-    }
-}
+  },
+};
