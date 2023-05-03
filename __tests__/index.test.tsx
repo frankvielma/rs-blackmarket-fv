88fc95dcaa0a9a7328b4d04bee771209eca7c728
Frank Vielma <frankvielma@gmail.com>
2023-05-03 14:26:11 -0400

    add testing (#8)

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react'
+import Home from '@/pages/index'
+
+describe('Home', () => {
+  it('renders a heading', () => {
+    render(<Home />)
+
+    const title = screen.getByText(/Get started by editing/i)
+  
+    expect(title).toBeInTheDocument()
+  })
+})
