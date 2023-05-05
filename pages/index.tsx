d672b139975e44fefeba82642c2c09e8bcaaad9f
Frank Vielma <frankvielma@gmail.com>
2023-05-05 12:56:30 -0400

    addtl updates on styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 import Image from 'next/image'
 
 const RsLogo = () => (
-  <div className="mt-24 flex justify-center">
+  <div className="mt-16 sm:mt-28">
     <Image src="/images/rs_logo.png" height={162} width={162} alt="Rs Logo" />
   </div>
 )
 
 const Collaborators = () => (
-  <div className="mt-10 flex justify-center">
+  <div className="mt-10">
     <Image
       src="/images/bg_collaborator.png"
       height={144}
@@ -19,14 +19,14 @@ const Collaborators = () => (
 
 export default function Home() {
   return (
-    <main className="flex h-[100vh] flex-col items-center bg-black text-white">
+    <main className="flex flex-col items-center text-center text-white">
       <RsLogo />
-      <div className="mt-36 flex text-5xl font-semibold">Black Market</div>
-      <div className="mt-20 flex justify-center rounded-xl bg-pink-600 p-9 py-6 text-5xl">
+      <div className="mt-32 text-5xl font-semibold sm:mt-40">Black Market</div>
+      <div className="mt-20 rounded-xl bg-pink-600 p-9 py-6 text-5xl sm:mt-32">
         UI
       </div>
-      <div className="mt-28 flex text-2xl font-normal">Collaborators</div>
-      <div className="mt-16 w-16 bg-[url('/images/bg_collaborator.png')] bg-no-repeat p-4 text-2xl font-semibold text-black">
+      <div className="mt-28 text-2xl font-normal sm:mt-32">Collaborators</div>
+      <div className="mt-14 w-16 bg-[url('/images/bg_collaborator.png')] bg-no-repeat p-4 text-2xl font-semibold text-black">
         FV
       </div>
     </main>
