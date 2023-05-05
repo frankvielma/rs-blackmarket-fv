dce467f8155ce584b6cf84dafa012f1df5e35b21
Frank Vielma <frankvielma@gmail.com>
2023-05-05 10:57:59 -0400

    add prettier-plugin-tailwindcss fix format

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 import Image from 'next/image'
 
 const RsLogo = () => (
-  <div className="flex justify-center mt-24">
+  <div className="mt-24 flex justify-center">
     <Image src="/images/rs_logo.png" height={162} width={162} alt="Rs Logo" />
   </div>
 )
 
 const Collaborators = () => (
-  <div className="flex justify-center mt-10">
+  <div className="mt-10 flex justify-center">
     <Image
       src="/images/bg_collaborator.png"
       height={144}
@@ -19,14 +19,14 @@ const Collaborators = () => (
 
 export default function Home() {
   return (
-    <main className="h-[100vh] bg-black flex flex-col items-center text-white">
+    <main className="flex h-[100vh] flex-col items-center bg-black text-white">
       <RsLogo />
-      <div className="flex mt-36 text-5xl font-semibold">Black Market</div>
-      <div className="mt-20 rounded-xl flex p-9 py-6 justify-center text-5xl bg-pink-600">
+      <div className="mt-36 flex text-5xl font-semibold">Black Market</div>
+      <div className="mt-20 flex justify-center rounded-xl bg-pink-600 p-9 py-6 text-5xl">
         UI
       </div>
-      <div className="flex mt-28 font-normal text-2xl">Collaborators</div>
-      <div className="mt-16 bg-[url('/images/bg_collaborator.png')] bg-no-repeat w-16 text-black p-4 font-semibold text-2xl">
+      <div className="mt-28 flex text-2xl font-normal">Collaborators</div>
+      <div className="mt-16 w-16 bg-[url('/images/bg_collaborator.png')] bg-no-repeat p-4 text-2xl font-semibold text-black">
         FV
       </div>
     </main>
