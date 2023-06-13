5ee71f6ffcad7a8b2e6d7fd419dbf7e688205fc6
Frank Vielma <frankvielma@gmail.com>
2023-06-12 21:11:27 -0400

    add password validation

--- components/InputPassword.tsx
+++ components/InputPassword.tsx
@@ -14,6 +14,19 @@ export default function InputPassword({
     setActive(!isActive)
   }
 
+  const [validationMessage, setValidationMessage] = useState('')
+
+  const handleBlur = (event: React.FocusEvent<HTMLInputElement>) => {
+    const input = event.target
+    if (!input.validity.patternMismatch) {
+      input.setCustomValidity('')
+    }
+    setValidationMessage(input.validationMessage)
+  }
+
+  const cssStyle =
+    'custom-focus h-[34px] w-auto rounded-md border border-dark-violet ps-3 placeholder:text-base placeholder:text-dark-gray hover:border-hover active:outline-none active:ring-2 active:ring-offset-2 md:h-[44px]'
+
   return (
     <div className="relative my-4">
       <label htmlFor="password" className="text-sm md:text-base">
@@ -21,14 +34,34 @@ export default function InputPassword({
       </label>
       <div className="flex flex-col">
         <input
-          className="custom-focus h-[34px] w-auto rounded-md border border-dark-violet ps-3 placeholder:text-base placeholder:text-dark-gray hover:border-hover active:outline-none active:ring-2 active:ring-offset-2 md:h-[44px]"
+          className={`border ${
+            validationMessage ? 'border-red-600 ' + cssStyle : cssStyle
+          }`}
           id={id}
           placeholder={placeholder}
           type={isActive ? 'password' : 'text'}
           onChange={onChange}
           pattern=".{8,}"
           required={true}
+          onBlur={handleBlur}
         />
+
+        {validationMessage ? (
+          <div className="flex flex-row">
+            <div className="">
+              <Image
+                src="/icons/Exclude.svg"
+                height={0}
+                width={0}
+                className="w-full pr-1 pt-1"
+                alt=""
+              />
+            </div>
+            <div> {validationMessage}</div>
+          </div>
+        ) : (
+          <span></span>
+        )}
 
         <button
           className="custom-focus absolute right-4 mt-2 md:mt-4"
