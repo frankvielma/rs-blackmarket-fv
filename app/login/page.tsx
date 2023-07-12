cdbf459744fc5aa56988948897295213794b209c
Frank Vielma <frankvielma@gmail.com>
2023-07-12 10:07:41 -0400

    add an h1 "Login" title that is visually hidden but available for screen readers

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -43,6 +43,7 @@ export default function Login() {
   return (
     <main className="flex h-[100vh] flex-col bg-[url('/images/bm_wallpaper.jpg')] bg-cover bg-center bg-no-repeat px-6 py-16 text-base text-dark-violet md:px-28">
       <div className="h-[366px] max-w-[328px] rounded-lg border-black bg-white px-[34px] md:h-[425px] md:max-w-[360px]">
+        <h1 className="sr-only">Login</h1>
         <div className="pb-5 pt-[40px] md:p-[47px] md:pb-7">
           <Logo />
         </div>
