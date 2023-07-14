a025441c52d83114dbf8e02aaf3c98477cb537b3
Frank Vielma <frankvielma@gmail.com>
2023-07-14 12:09:38 -0400

    update login tests

--- __tests__/login.tsx
+++ __tests__/login.tsx
@@ -1,7 +1,22 @@
-import { render } from '@testing-library/react'
+import { render, fireEvent, screen } from '@testing-library/react'
 import Login from '@/app/login/page'
 
-it('renders login unchanged', () => {
-  const { container } = render(<Login />)
-  expect(container).toMatchSnapshot()
+describe('Login', () => {
+  it('renders login unchanged', () => {
+    const { container } = render(<Login />)
+    expect(container).toMatchSnapshot()
+  }),
+    it('should enable login button', () => {
+      render(<Login />)
+      const emailInput = screen.getByLabelText('Email')
+      const passwordInput = screen.getByLabelText('Password')
+
+      fireEvent.change(emailInput, { target: { value: 'test@example.com' } })
+      fireEvent.change(passwordInput, { target: { value: 'MyPassword123' } })
+
+      const buttonElement = screen.getByRole('button', { name: 'Log in' })
+      expect(buttonElement).toHaveClass(
+        'active:ring-2 active:ring-offset-2 active:outline-none custom-focus hover:bg-hover active:bg-hover'
+      )
+    })
 })
